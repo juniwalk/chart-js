f21219ed5ce8ef34c5fb8f412c8d2277effa5a70
juniwalk <juniwalk@outlook.cz>
2026-02-07 21:49:51 +0100

    Allow array<string, float> as data input

--- src/DataSet.php
+++ src/DataSet.php
@@ -10,14 +10,14 @@ namespace JuniWalk\ChartJS;
 use Stringable;
 
 /**
- * @phpstan-type KeyValuePairs array<array{key: string, value: int|float}>
+ * @phpstan-type KeyValuePairs array<string, int|float>|array<array{key: string, value: int|float}>
  */
 interface DataSet extends OptionHandler
 {
 	public function getAverage(): float;
 
 	/**
-	 * @return array{data: array<string, int|float>|KeyValuePairs, label?: string|int|float|Stringable}
+	 * @return array{data: KeyValuePairs, label?: string|int|float|Stringable}
 	 */
 	public function createConfig(): array;
 }
